2c5e508eb37f788fdb276e6200ac5e9e1671abd1
tomassirio <tomassirio@gmail.com>
2024-04-27 15:33:34 +0200

    First commit

---
+++ common.h
@@ -0,0 +1,12 @@
+//
+// Created by Tomas Sirio on 27/04/2024.
+//
+
+#ifndef CLOX_COMMON_H
+#define CLOX_COMMON_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif //CLOX_COMMON_H
